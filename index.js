9d1681bb4e338feef7904f31cb367130b2557429
iceStone <ice@micua.com>
2015-07-22 14:10:04 +0800

    2015-07-22

---
+++ index.js
@@ -0,0 +1,10 @@
+/*
+* @Author: iceStone
+* @Date:   2015-07-18 21:56:00
+* @Last Modified by:   iceStone
+* @Last Modified time: 2015-07-18 22:27:49
+*/
+
+'use strict';
+
+module.exports = require('./lib/micua');
